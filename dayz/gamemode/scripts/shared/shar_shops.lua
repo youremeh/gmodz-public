6a09edc6167074c5d4b12170dbb12ee4cb14f91a
youremeh <youremeh@proton.me>
2023-07-24 19:57:25 -0400

    Update shop items
    All weapons
    All crafting
    All survival gear

--- dayz/gamemode/scripts/shared/shar_shops.lua
+++ dayz/gamemode/scripts/shared/shar_shops.lua
@@ -3,8 +3,8 @@
     You must use the Item ID
 ]]
 DayZShop = {}
-DayZShop["shop_weapons"] = {41, 44, 45, 52, 46, 53} --Weapons
-DayZShop["shop_equipment"] = {60, 61, 62, 63, 64} --Equipment
-DayZShop["shop_barricades"] = {65, 66, 67, 68, 69} --Crafting
-DayZShop["shop_gear"] = {13, 14, 15, 22, 24, 70} --Survival Gear
+DayZShop["shop_weapons"] = {47, 48, 49, 50, 51, 52, 53, 54, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70} --Weapons
+DayZShop["shop_equipment"] = {36, 37, 38, 39, 40} --Equipment
+DayZShop["shop_gear"] = {13, 14, 15, 22, 24} --Survival Gear
+DayZShop["shop_barricades"] = {41, 42, 43, 44, 45} --Crafting
 DayZShop["shop_perks"] = {16, 17, 18, 19, 20} --Perks
